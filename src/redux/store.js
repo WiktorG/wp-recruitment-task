b31caf8ccfdb350bd5067e9d9c7782faa2d04765
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-05-28 21:51:40 +0200

    fix: devtools fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,16 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from '~/redux/reducers/rootReducer';
 import rootSaga from '~/redux/sagas/rootSaga';
 
+// I leave it so that you can check it in your browser on production :)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__() : compose;
+
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     rootReducer,
     compose(
         applyMiddleware(sagaMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        devTools,
     ),
 );
 
