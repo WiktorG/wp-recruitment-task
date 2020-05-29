c10c814940c435198a3ac42db857e7a34b5e4c5d
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-05-29 13:00:40 +0200

    feat: action to increase cart items create

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,9 +1,16 @@
+import { INCREASE_CART_ITEM_COUNT } from '~/redux/actions/cartActions';
+
 const initialState = {
     itemCount: 0,
 };
 
 const cartReducer = (state = initialState, action) => {
     switch (action.type) {
+    case INCREASE_CART_ITEM_COUNT:
+        return {
+            ...state,
+            itemCount: state.itemCount + action.amountToIncrease,
+        };
     default:
         return state;
     }
