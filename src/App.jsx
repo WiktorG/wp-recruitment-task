f147087b56ea477eddbfb7668224f4ad64cdf18a
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-05-29 14:39:55 +0200

    refactor: delete theme provider because not used

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { ThemeProvider } from 'styled-components';
 import { IconContext } from 'react-icons';
-import theme from '~/styled/theme';
 import GlobalStyles from '~/styled/GlobalStyles';
 
 import client from '~/graphql/client';
@@ -16,14 +14,12 @@ import Footer from '~/components/Footer';
 const App = () => (
     <ApolloProvider client={client}>
         <Provider store={store}>
-            <ThemeProvider theme={theme}>
-                <IconContext.Provider value={{ className: 'react-icons' }}>
-                    <Nav />
-                    <ShipList />
-                    <Footer />
-                    <GlobalStyles />
-                </IconContext.Provider>
-            </ThemeProvider>
+            <IconContext.Provider value={{ className: 'react-icons' }}>
+                <Nav />
+                <ShipList />
+                <Footer />
+                <GlobalStyles />
+            </IconContext.Provider>
         </Provider>
     </ApolloProvider>
 );
